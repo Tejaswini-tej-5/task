d7e8d1eafb33d941e8792f8a5f43dc1cc1d07b20
Tejaswini-tej-5 <tejaswini.6786@gmail.com>
2021-03-22 16:49:21 +0530

    changing

--- pyinstall.py
+++ pyinstall.py
@@ -1,6 +1,8 @@
+import time
 x = input('Enter value of x: ')
 y = input('Enter value of y: ')
 x,y=y,x
 
 print('The value of x after swapping:'+str(x))
 print('The value of y after swapping:'+ str(y))
+time.sleep(30)
